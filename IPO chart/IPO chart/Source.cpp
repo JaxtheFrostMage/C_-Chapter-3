f6095b80d82902a04942c6581ba7b9c166bcd29c
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-08-29 10:50:10 -0500

    working

--- IPO chart/IPO chart/Source.cpp
+++ IPO chart/IPO chart/Source.cpp
@@ -1,8 +1,23 @@
-#include (iostream)
-#include (string)
-using namespace std; 
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-	
+	double dollars;
+	double pounds;
+	double pesos;
+	double yen;
+
+	cout << "How many american dollars do you have? ";
+	cin >> dollars;
+	pounds = dollars * 0.6318;
+	pesos = dollars * 12.8863;
+	yen = dollars * 82.34;
+	cout << dollars << " american dollars covnerts to " << pounds << " pounds" << endl;
+	cout << dollars << " american dollars covnerts to " << pesos << " pesos" << endl;
+	cout << dollars << " american dollars covnerts to " << yen << " yen" << endl;
+
+	system("pause");
+	return 0;
 }
