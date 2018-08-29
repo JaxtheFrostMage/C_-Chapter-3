7d9d743487cd9bed27867a2ec5db34dba0294c33
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-08-29 10:34:14 -0500

    starting IPO

--- C++_mad_libs/C++_mad_libs/Source.cpp
+++ C++_mad_libs/C++_mad_libs/Source.cpp
@@ -4,37 +4,14 @@ using namespace std;
 
 int main()
 {
-	string color;
-	string wordEndingInEst;
-	string anAnimal;
-	string bodyPartPlural;
-	string aNoun;
-	string pluralNoun;
-	int a;
-	int b;
-	int c;
-	
+	double dollars;
+	//double pounds;
+	//double pesos;
+	//double yen;
 
-	cout << "What is your favorite color? " << endl;
-	cin >> color; 
-	cout << "Name a word that ends with 'est' " << endl;
-	cin >> wordEndingInEst;
-	cout << "What is your favorite animal? " << endl;
-	cin >> anAnimal;
-	cout << "Name a body part. " << endl;
-	cin >> bodyPartPlural;
-	cout << "Name a noun. " << endl;
-	cin >> aNoun;
-	cout << "Name a plural noun. " << endl;
-	cin >> pluralNoun;
-	cout << "Give a number. " << endl;
-	cin >> a;
-	cout << "Give another number. " << endl;
-	cin >> b;
+	cout << "How many american dollars do you have? ";
+	cin >> dollars;
 
-	c = a - b;
-
-	cout << "The " << color << " bird is the " << wordEndingInEst << " avian beast in the lands. it has " << c << " " << bodyPartPlural << ", and a oddly shaped " << aNoun << ". its favorite meals include " << anAnimal << " and " << pluralNoun << " , though they still enjoy a good salad." << endl;
 
 	system("pause");
 	return 0;
