987e9a07f6cfd6334c8ab7840e6ddf8cee0f2dee
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-08-31 10:01:35 -0500

    start

--- Temp. Conversion/Temp. Conversion/Source.cpp
+++ Temp. Conversion/Temp. Conversion/Source.cpp
@@ -1,1 +1,13 @@
 //Tempature Conversion
+//Author: Evan Raker
+//Date: 8/30/2018
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int temp1 =0;
+	int temp2 =0;
+	int temp3 =0;
+}
